c268dfa026619583f509262c2be0b58c28927b69
RealGamertime <blueant15@gmail.com>
2024-01-20 22:48:47 -0800

    Start game button now works.

--- MIAModManager/Form1.Designer.cs
+++ MIAModManager/Form1.Designer.cs
@@ -34,43 +34,45 @@
             button4 = new System.Windows.Forms.Button();
             listBox1 = new System.Windows.Forms.ListBox();
             textBox1 = new System.Windows.Forms.TextBox();
-            Locate_Game = new System.Windows.Forms.Button();
             Start_Game = new System.Windows.Forms.Button();
-            textBox2 = new System.Windows.Forms.TextBox();
             SuspendLayout();
             // 
             // Add_Mod
             // 
-            Add_Mod.Location = new System.Drawing.Point(12, 511);
+            Add_Mod.Location = new System.Drawing.Point(10, 383);
+            Add_Mod.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             Add_Mod.Name = "Add_Mod";
-            Add_Mod.Size = new System.Drawing.Size(100, 30);
+            Add_Mod.Size = new System.Drawing.Size(88, 22);
             Add_Mod.TabIndex = 0;
             Add_Mod.Text = "Add";
             Add_Mod.UseVisualStyleBackColor = true;
             // 
             // Remove_Mod
             // 
-            Remove_Mod.Location = new System.Drawing.Point(118, 511);
+            Remove_Mod.Location = new System.Drawing.Point(103, 383);
+            Remove_Mod.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             Remove_Mod.Name = "Remove_Mod";
-            Remove_Mod.Size = new System.Drawing.Size(100, 30);
+            Remove_Mod.Size = new System.Drawing.Size(88, 22);
             Remove_Mod.TabIndex = 1;
             Remove_Mod.Text = "Remove";
             Remove_Mod.UseVisualStyleBackColor = true;
             // 
             // Order_Up
             // 
-            Order_Up.Location = new System.Drawing.Point(224, 511);
+            Order_Up.Location = new System.Drawing.Point(196, 383);
+            Order_Up.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             Order_Up.Name = "Order_Up";
-            Order_Up.Size = new System.Drawing.Size(100, 30);
+            Order_Up.Size = new System.Drawing.Size(88, 22);
             Order_Up.TabIndex = 2;
             Order_Up.Text = "Order up";
             Order_Up.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            button4.Location = new System.Drawing.Point(330, 511);
+            button4.Location = new System.Drawing.Point(289, 383);
+            button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             button4.Name = "button4";
-            button4.Size = new System.Drawing.Size(100, 30);
+            button4.Size = new System.Drawing.Size(88, 22);
             button4.TabIndex = 3;
             button4.Text = "Order down";
             button4.UseVisualStyleBackColor = true;
@@ -78,65 +80,49 @@
             // listBox1
             // 
             listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 20;
-            listBox1.Location = new System.Drawing.Point(12, 21);
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new System.Drawing.Point(10, 16);
+            listBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             listBox1.Name = "listBox1";
-            listBox1.Size = new System.Drawing.Size(418, 484);
+            listBox1.Size = new System.Drawing.Size(366, 364);
             listBox1.TabIndex = 4;
             // 
             // textBox1
             // 
-            textBox1.Location = new System.Drawing.Point(480, 21);
+            textBox1.Location = new System.Drawing.Point(420, 16);
+            textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new System.Drawing.Size(514, 381);
+            textBox1.Size = new System.Drawing.Size(450, 287);
             textBox1.TabIndex = 5;
             textBox1.Text = "Maybe Info of selected mod here?";
             // 
-            // Locate_Game
-            // 
-            Locate_Game.Location = new System.Drawing.Point(480, 455);
-            Locate_Game.Name = "Locate_Game";
-            Locate_Game.Size = new System.Drawing.Size(200, 50);
-            Locate_Game.TabIndex = 6;
-            Locate_Game.Text = "Locate game";
-            Locate_Game.UseVisualStyleBackColor = true;
-            // 
             // Start_Game
             // 
-            Start_Game.Location = new System.Drawing.Point(794, 455);
+            Start_Game.Location = new System.Drawing.Point(695, 341);
+            Start_Game.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             Start_Game.Name = "Start_Game";
-            Start_Game.Size = new System.Drawing.Size(200, 50);
+            Start_Game.Size = new System.Drawing.Size(175, 38);
             Start_Game.TabIndex = 7;
             Start_Game.Text = "Start game";
             Start_Game.UseVisualStyleBackColor = true;
-            // 
-            // textBox2
-            // 
-            textBox2.BackColor = System.Drawing.SystemColors.Control;
-            textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            textBox2.Location = new System.Drawing.Point(480, 511);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new System.Drawing.Size(200, 20);
-            textBox2.TabIndex = 8;
-            textBox2.Text = "Game located/not located";
+            Start_Game.Click += Start_Game_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(1006, 553);
-            Controls.Add(textBox2);
+            ClientSize = new System.Drawing.Size(882, 421);
             Controls.Add(Start_Game);
-            Controls.Add(Locate_Game);
             Controls.Add(textBox1);
             Controls.Add(listBox1);
             Controls.Add(button4);
             Controls.Add(Order_Up);
             Controls.Add(Remove_Mod);
             Controls.Add(Add_Mod);
-            MaximumSize = new System.Drawing.Size(1024, 600);
-            MinimumSize = new System.Drawing.Size(1024, 600);
+            Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            MaximumSize = new System.Drawing.Size(898, 460);
+            MinimumSize = new System.Drawing.Size(898, 460);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -151,9 +137,7 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button Locate_Game;
         private System.Windows.Forms.Button Start_Game;
-        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- MIAModManager/Form1.cs
+++ MIAModManager/Form1.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using Microsoft.Win32;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace MIAModManager
 {
@@ -15,7 +16,16 @@ namespace MIAModManager
         public Form1()
         {
             InitializeComponent();
+            
         }
 
+        private void Start_Game_Click(object sender, EventArgs e)
+        {
+            string executablePath = GameUtils.GetGameRootDirectory() + "\\MadeInAbyss.exe";
+            if (File.Exists(executablePath))
+                Process.Start(executablePath);
+
+        }
+        
     }
 }
